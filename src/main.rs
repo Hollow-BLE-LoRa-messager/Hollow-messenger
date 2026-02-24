5cf67b4acbb41dc39ac199d930038b54f622bdf3
MozzarellaCheesee <danilvasilkov59@gmail.com>
2026-02-25 02:41:57 +0300

    All values ​​in app-config.slint are configured

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@ slint::include_modules!();
 fn main() -> Result<(), slint::PlatformError> {
     let main_window = AppWindow::new()?;
 
-    main_window.on_text_changed({
     let main_window_handle = main_window.as_weak();
-    move |new_text| {
-        let main_window = main_window_handle.unwrap();
-        let limited: String = new_text.chars().take(50).collect();
-        main_window.set_search_text(limited.into());
-    }
-});
+    main_window.on_text_changed({move |new_text| {
+            let main_window = main_window_handle.unwrap();
+            let limited: String = new_text.chars().take(50).collect();
+            main_window.set_search_text(limited.into());
+        }
+    });
 
     main_window.run()
 }
